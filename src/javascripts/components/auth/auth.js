7de7804956aa5756a60f807c0090abcf33825f14
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-05-26 14:53:45 -0500

    login and register

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,9 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import util from '../../helpers/util';
 import googleImage from './google-login-button.png';
 
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
 const authStringBuilder = () => {
-  const domString = 'auth';
+  let domString = '<button id="google-auth" class="btn btn-danger">';
+  domString += `<img src=${googleImage}/>`;
+  domString += '</button>';
   util.printToDom('auth', domString);
+  document.getElementById('google-auth').addEventListener('click', signMeIn);
 };
 
 export default { authStringBuilder };
