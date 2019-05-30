f74c4de0c0b729d7f28693d4bae6b1b47746d417
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-05-30 18:24:06 -0500

    movies printing to the dom

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,17 +2,15 @@ import firebase from 'firebase/app';
 import '../styles/main.scss';
 import 'bootstrap';
 import auth from './components/auth/auth';
-import movie from './components/movies/movies';
-import apiKeys from './helpers/apiKeys.json';
 import MyNavbar from './components/MyNavbar/MyNavbar';
 import authData from './helpers/data/authData';
+import apiKeys from './helpers/apiKeys.json';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   MyNavbar.navbarEvents();
   authData.checkLoginStatus();
   auth.authStringBuilder();
-  movie.movieStringBuilder();
 };
 
 init();
